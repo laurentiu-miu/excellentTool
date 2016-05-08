a981e6fd1c97e778c0665303c866d0b77301853c
laurentiumiu <miulaurctin@gmail.com>
2016-05-08 23:06:49 +0300

    event for list

--- src/main/web/app/hello/hello.component.ts
+++ src/main/web/app/hello/hello.component.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import {Component, Inject} from "angular2/core";
+import {Component, Inject, EventEmitter} from "angular2/core";
 import {CORE_DIRECTIVES} from "angular2/common";
 import {RouterLink} from "angular2/router";
 import {HelloService} from "./hello.service";
@@ -11,15 +11,22 @@ import {Demo} from "./Demo";
     selector: 'test',
     templateUrl: 'app/hello/hello.component.html',
     providers: [HelloService],
-    directives: [CORE_DIRECTIVES, RouterLink]
+    directives: [CORE_DIRECTIVES, RouterLink],
+    outputs: ['selectedDemo']
 })
 export class HelloComponent {
 
     private jsonResponse: string;
     private message: string;
     private demos:Array<Demo>;
+    selectedDemo = new EventEmitter();
 
     constructor(@Inject(HelloService) private helloService: HelloService) {}
+
+    onSelect(demo:Demo) {
+        console.log(JSON.stringify(demo));
+        this.selectedDemo.emit(demo);
+    }
 
     ngOnInit() {
         this.helloService.getTest().subscribe(
